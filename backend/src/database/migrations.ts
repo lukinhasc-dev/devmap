b55f8235d33c3f4ee183724c570f864e47721aa3
lukinhasc-dev <lucas.sca2022@gmail.com>
2026-03-02 16:54:22 -0300

    feat: adicionado banco de dados do database, tasks e github direto

--- backend/src/database/migrations.ts
+++ backend/src/database/migrations.ts
@@ -1,10 +1,13 @@
 import db from "./connection";
 
-db.pragma("foreign_keys = ON");
-
 export function runMigrations() {
+    db.pragma("foreign_keys = ON");
+
     projects();
     endpoints();
+    databases();
+    github();
+    tasks();
 }
 
 export function endpoints() {
@@ -46,4 +49,72 @@ export function projects() {
     console.log("🚀 Tabela projects criada!");
 }
 
+export function databases() {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS databases (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            project_id INTEGER NOT NULL,
 
+            nome TEXT NOT NULL,
+            tipo_bd TEXT NOT NULL, -- mysql, postgres, sqlite, mongo
+            host TEXT,
+            porta TEXT,
+            query_bd TEXT, -- Query do banco de dados para facilitar entendimento
+            usuario TEXT,
+            senha TEXT,
+            observacoes TEXT,
+
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY (project_id)
+                REFERENCES projects(id)
+                ON DELETE CASCADE
+        );
+    `);
+
+    console.log("🚀 Tabela databases criada!");
+}
+
+export function github() {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS github_repos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            project_id INTEGER NOT NULL,
+
+            nome_repositorio TEXT NOT NULL,
+            link_repositorio TEXT NOT NULL,
+            stack TEXT,
+            observacoes TEXT,
+
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY (project_id)
+                REFERENCES projects(id)
+                ON DELETE CASCADE
+        );
+    `);
+
+    console.log("🚀 Tabela github criada!");
+}
+
+
+export function tasks() {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            project_id INTEGER NOT NULL,
+
+            titulo TEXT NOT NULL,
+            descricao TEXT,
+            status TEXT NOT NULL, -- pendente, em andamento, concluida
+
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY (project_id)
+                REFERENCES projects(id)
+                ON DELETE CASCADE
+        );
+    `);
+
+    console.log("🚀 Tabela tasks criada!");
+}
