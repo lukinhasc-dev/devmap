ca4fb3497808cdc708f16831cdeeb59e82d18668
lukinhasc-dev <lucas.sca2022@gmail.com>
2026-03-06 12:09:16 -0300

    feat: adicionado service no frontend do schemas

---
+++ frontend/src/services/schema.service.ts
@@ -0,0 +1,13 @@
+import api from "./api";
+
+export default class SchemaService {
+    async getSchemaByDatabaseId(id: number) {
+        const response = await api.get(`/databases/${id}/schema`);
+        return response.data;
+    }
+
+    async parseSchema(sql: string) {
+        const response = await api.post(`/schema/parse`, { sql });
+        return response.data;
+    }
+}
